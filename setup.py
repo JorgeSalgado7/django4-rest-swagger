bc3b76b2f5e858be9dd153388d458b0597cb0d3b
JorgeSalgado7 <j.salgado7@outlook.com>
2023-01-25 09:55:34 -0600

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ pip install django4-rest-swagger
 """
 
 setup(
-	name='django4-rest-fswagger',
+	name='django4-rest-swagger',
 	version=VERSION,
 	install_requires=[
 		'Django>=4.1.5',
@@ -22,12 +22,24 @@ setup(
 		'openapi-codec>=1.3.2',
 		'simplejson',
 	],
-	packages=['django4-rest-swagger'],
+	packages=['django4_rest_swagger'],
 	include_package_data=True,
     license='FreeBSD License',
     description='Swagger UI for Django 4 & Django REST Framework 3+',
     long_description=README,
     author='Jorge Salgado',
     author_email='contacto@jorgesalgado.com.mx',
-	url='https://github.com/JorgeSalgado7/django4-rest-swagger'
+	url='https://github.com/JorgeSalgado7/django4-rest-swagger',
+	classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4+',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10+',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
